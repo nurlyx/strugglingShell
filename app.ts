cf10fc58e3dfc8706dda801343bbde543d500357
nurlyx <206698572+nurlyx@users.noreply.github.com>
2025-05-24 23:45:11 +0530

    remove all the bulitin stuff

--- app.ts
+++ app.ts
@@ -1,10 +1,8 @@
 import { App } from "astal/gtk3"
-import style from "./style.scss"
 import Bar from "./widget/Bar"
 
 App.start({
-    css: style,
-    main() {
-        App.get_monitors().map(Bar)
-    },
+  main() {
+    Bar(0)
+  },
 })
